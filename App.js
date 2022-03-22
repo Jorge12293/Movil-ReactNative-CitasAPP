c2a39de6d231ec3e314a6cca6892c5db3f9a63c7
Jorge <jorgerivera12293@gmail.com>
2022-03-22 02:43:54 -0500

    Creacion de interfaz principal

--- App.js
+++ App.js
@@ -1,27 +1,85 @@
 
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import {
   SafeAreaView,
-  Text
+  Text,
+  Pressable,
+  Modal,
+  StyleSheet
 } from 'react-native';
 
 
 
 const App= () => {
 
+  const [modalVisible,setModalVisible]= useState(false);
+
+
+  const nuevaCitaHandler = () =>{
+    setModalVisible(true);
+    console.log('Diste click...');
+  }
 
   return (
-    <SafeAreaView>
-      <Text>  Administrador de Citas  
-        <Text> Veterinaria</Text>
+    <SafeAreaView style={styles.container}>
+
+      <Text style={styles.titulo}>  Administrador de Citas  
+        <Text style={styles.tituloBold}> Veterinaria</Text>
       </Text>
       
+      <Pressable
+        style={styles.btnNuevaCita}
+        onPress={nuevaCitaHandler}
+        
+      >
+        <Text style={styles.btnTextNuevaCita}>Nueva Cita</Text>
+      </Pressable>
+      
+      <Modal
+        animationType='slide'
+        visible={modalVisible}
+      >
+        <Text>Desde Modal</Text>
+      </Modal>
+
     </SafeAreaView>
     
   );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'#F3F4F6',
+    flex: 1
+  },
+  titulo:{
+    textAlign:'center',
+    textTransform:'uppercase',
+    fontSize:30,
+    color:'#374151',
+    fontWeight:'600'
+  },
+  tituloBold:{
+    fontWeight:'900',
+    color:'#6D28D9',
+  },
+  btnNuevaCita:{
+    backgroundColor:'#6D28D9',
+    padding:15,
+    marginTop:30,
+    marginHorizontal:20,
+    borderRadius:10
+
+  },
+  btnTextNuevaCita:{
+    textAlign:'center',
+    color: '#FFF',
+    fontSize:18,
+    fontWeight:'900'
+  }
+})
 
 
 export default App;
