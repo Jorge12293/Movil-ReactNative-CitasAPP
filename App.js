e4e76ef7f58f8688ddc10dbef49d63757f3ffdd1
Jorge <jorgerivera12293@gmail.com>
2022-03-22 01:35:38 -0500

    Primer Commit

---
+++ App.js
@@ -0,0 +1,27 @@
+
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+
+
+const App= () => {
+
+
+  return (
+    <SafeAreaView>
+      <Text>  Administrador de Citas  
+        <Text> Veterinaria</Text>
+      </Text>
+      
+    </SafeAreaView>
+    
+  );
+};
+
+
+export default App;
